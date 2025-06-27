610e4170a5302c1a63cf8472dd73285d56b8ca1a
sukhansidhu <jashanpreetsingh.chotian@gmail.com>
2025-06-27 22:31:54 +0530

    Update init.py

--- handlers/media_tools/init.py
+++ handlers/media_tools/init.py
@@ -1,5 +1,3 @@
-from pyrogram import filters
-from pyrogram.handlers import CallbackQueryHandler
 from .caption_editor import caption_editor_handler
 from .metadata_editor import metadata_editor_handler
 from .forwarder import forwarder_handler
@@ -13,18 +11,19 @@ from .renamer import renamer_handler
 from .media_info import media_info_handler
 from .archiver import archiver_handler
 
-def media_tools_handler():
-    return [
-        caption_editor_handler(),
-        metadata_editor_handler(),
-        forwarder_handler(),
-        stream_tools_handler(),
-        video_trimmer_handler(),
-        video_merger_handler(),
-        audio_tools_handler(),
-        screenshot_handler(),
-        converter_handler(),
-        renamer_handler(),
-        media_info_handler(),
-        archiver_handler()
-    ]
+def get_media_handlers():
+    """Return all media tools handlers as a flat list"""
+    handlers = []
+    handlers.extend(caption_editor_handler())
+    handlers.extend(metadata_editor_handler())
+    handlers.extend(forwarder_handler())
+    handlers.extend(stream_tools_handler())
+    handlers.extend(video_trimmer_handler())
+    handlers.extend(video_merger_handler())
+    handlers.extend(audio_tools_handler())
+    handlers.extend(screenshot_handler())
+    handlers.extend(converter_handler())
+    handlers.extend(renamer_handler())
+    handlers.extend(media_info_handler())
+    handlers.extend(archiver_handler())
+    return handlers
