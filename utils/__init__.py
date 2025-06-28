efc8d097ec6bc3d95e4d80d05a80fa9ddc9ea57e
sukhansidhu <jashanpreetsingh.chotian@gmail.com>
2025-06-28 12:49:13 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,6 @@
-from .buttons import *
+from .buttons import get_media_options, back_button, ...  # only what you use
 from .db import Database
-from .ffmpeg import *
+from .ffmpeg import trim_video, convert_video, extract_audio  # example functions
 # Don't import progress_callback here to avoid circular imports
+
+__all__ = ["Database", "get_media_options", "back_button", "trim_video", "convert_video", "extract_audio"]
